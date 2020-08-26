c7989c8a5e3cc68bab76395c9de087be8c5ab274
leovdn <leo_vdn@hotmail.com>
2020-08-25 21:14:27 -0300

    configured debbuger and user test

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
-import express from 'express';
+import express, { Router, json } from 'express';
 import routes from './routes';
+
+const routes = Router();
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.send({ message: 'Hello GoStack' }),
-);
+app.use(json());
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  response.json(user);
+});
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333!');
